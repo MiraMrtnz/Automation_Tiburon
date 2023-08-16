395a721289b85d97bc017ee5bdabab4d5dd8cf27
miramrtnz <mira.mrtnz@gmail.com>
2023-08-16 14:55:06 -0700

    innit

---
+++ path.py
@@ -0,0 +1,11 @@
+import os
+import time
+import pyautogui
+
+time.sleep(2)
+print(pyautogui.position())
+
+#pyautogui.doubleClick(680,466)
+#pyautogui.hotkey('ctrl', 'c')
+#pyautogui.doubleClick(2200,905)
+#pyautogui.hotkey('ctrl', 'v')
