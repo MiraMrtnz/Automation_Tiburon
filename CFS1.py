5ccbce629069899e1408464a6c4d7af91b921ab2
MiraMrtnz <91913323+MiraMrtnz@users.noreply.github.com>
2023-08-16 15:14:32 -0700

    CFS1.py
    add documentation for added features

--- CFS1.py
+++ CFS1.py
@@ -21,11 +21,12 @@ time.sleep(1)
 #pyautogui.hotkey('ctrl', 'c')
 pyautogui.press('e')
 
-
+#Enter Tiburon Interface
 time.sleep(1)
 pyautogui.click(2167,617)
 #time.sleep(1)
 
+#SP/CFS manual edits
 pyautogui.click(2115,907)
 pyautogui.click(2114,904)
 #time.sleep(1)
@@ -34,6 +35,7 @@ pyautogui.press('backspace')
 time.sleep(1)
 pyautogui.click(2199,906)
 
+#Successful ST feature
 pyautogui.press('backspace')
 pyautogui.press('backspace')
 pyautogui.press('backspace')
@@ -45,15 +47,17 @@ time.sleep(1)
 
 
 
-
+#Automate start feature
 #pyautogui.click(3380,107)
 #pyautogui.hotkey('ctrl', 'NumPad_Decimal')
 
+#Automate a copy/paste feature
 #pyautogui.doubleClick(680,466)
 #pyautogui.hotkey('ctrl', 'c')
 #pyautogui.doubleClick(2200,905)
 #pyautogui.hotkey('ctrl', 'v')
 
+#Create a 'for loop'
 #pyautogui.press('backspace')
 #pyautogui.press('backspace')
 #time.sleep(1)
@@ -64,9 +68,10 @@ time.sleep(1)
 #pyautogui.press('backspace')
 #pyautogui.press('backspace')
 #time.sleep(2)
+
+#Formatting
 #pyautogui.press(')')
 #pyautogui.click(1041,473)
-
 #pyautogui.press('backspace')
 #pyautogui.click(1041,473)
 #pyautogui.press('end')
